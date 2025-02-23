7b5fe82f01d27e032271596c2c828658e97a1f4f
Daiyun199 <yenphung3000@gmail.com>
2025-02-24 01:11:51 +0700

    fix profile page + fetch api create account

--- kosij/next.config.ts
+++ kosij/next.config.ts
@@ -3,55 +3,34 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["static.wikia.nocookie.net", "firebasestorage.googleapis.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "static.wikia.nocookie.net",
+      },
+      {
+        protocol: "https",
+        hostname: "firebasestorage.googleapis.com",
+      },
+      {
+        protocol: "https",
+        hostname: "encrypted-tbn0.gstatic.com",
+      },
+    ],
   },
   async rewrites() {
     return [
-      {
-        source: "/customer-list",
-        destination: "/customerList",
-      },
-      {
-        source: "/tour-list",
-        destination: "/tourList",
-      },
-
-      {
-        source: "/account-create",
-        destination: "/accountCreate",
-      },
-      {
-        source: "/tour-pending",
-        destination: "/tourPending",
-      },
-      {
-        source: "/dashboard/finance",
-        destination: "/finance",
-      },
-      {
-        source: "/dashboard/orders",
-        destination: "/order",
-      },
-      {
-        source: "/dashboard/tour",
-        destination: "/tour",
-      },
-      {
-        source: "/order-list",
-        destination: "/orderList",
-      },
-      {
-        source: "/tour-detail",
-        destination: "/tourDetail",
-      },
-      {
-        source: "/delivery-staff-list",
-        destination: "/DList",
-      },
-      {
-        source: "/",
-        destination: "/login",
-      },
+      { source: "/customer-list", destination: "/customerList" },
+      { source: "/tour-list", destination: "/tourList" },
+      { source: "/account-create", destination: "/accountCreate" },
+      { source: "/tour-pending", destination: "/tourPending" },
+      { source: "/dashboard/finance", destination: "/finance" },
+      { source: "/dashboard/orders", destination: "/order" },
+      { source: "/dashboard/tour", destination: "/tour" },
+      { source: "/order-list", destination: "/orderList" },
+      { source: "/tour-detail", destination: "/tourDetail" },
+      { source: "/delivery-staff-list", destination: "/DList" },
+      { source: "/", destination: "/login" },
     ];
   },
 };

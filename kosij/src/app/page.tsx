e694de055d7efcda73bbe0527577b19f63e91f9a
Daiyun199 <yenphung3000@gmail.com>
2025-03-06 14:32:23 +0700

    fix

--- kosij/src/app/page.tsx
+++ kosij/src/app/page.tsx
@@ -1,14 +1,5 @@
-"use client";
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  const router = useRouter();
-
-  useEffect(() => {
-    router.push("/login");
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  return <p>Redirecting...</p>;
+  redirect("/login");
 }

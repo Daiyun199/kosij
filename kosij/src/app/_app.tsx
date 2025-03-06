0e2e563161851a780ef8fbf3d1d1fc885010f7b0
linhnnguyenngoc03 <linhnguyenngoc03@gmail.com>
2025-03-06 18:35:24 +0700

    push

--- kosij/src/app/_app.tsx
+++ kosij/src/app/_app.tsx
@@ -1,4 +1,5 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { App, ConfigProvider } from "antd";
 import { AppProps } from "next/app";
 import { useRef } from "react";
 import "@/styles/globals.css";
@@ -8,7 +9,11 @@ export default function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <QueryClientProvider client={queryClientRef.current}>
-      <Component {...pageProps} />
+      <ConfigProvider>
+        <App>
+          <Component {...pageProps} />
+        </App>
+      </ConfigProvider>
     </QueryClientProvider>
   );
 }
